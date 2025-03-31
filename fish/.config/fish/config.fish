01f901a65ba8c5541b7bfa2645fb24110a1d34b0
Rishabh Haldiya <rishabhhaldiya18@gmail.com>
2025-03-31 07:46:42 +0530

    s

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -159,3 +159,12 @@ end
 
 export WINEESYNC=1
 export WINEFSYNC=1
+
+
+# BEGIN opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+test -r '/home/ste/.opam/opam-init/init.fish' && source '/home/ste/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+# END opam configuration
