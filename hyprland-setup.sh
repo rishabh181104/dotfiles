a6715bc1961905c6ad87480eecd0704603df9b40
Rishabh Haldiya <rishabhhaldiya18@gmail.com>
2025-04-27 20:13:07 +0530

    s

--- hyprland-setup.sh
+++ hyprland-setup.sh
@@ -2,49 +2,49 @@
 
 # List of packages to install
 PACKAGES=(
-  hyprland
-  hyprlock
-  wl-clipboard
-  wf-recorder
-  rofi-wayland
-  pavucontrol
-  hyprland-qtutils
-  dunst
-  swaybg
-  alacritty
-  hyprcursor
-  noto-fonts
-  hyprpicker
-  nwg-look
-  jq
-  gvfs
-  mpv
-  playerctl
-  pamixer
-  neovim
-  brightnessctl
-  power-profiles-daemon
-  waybar
-  papirus-icon-theme
-  starship
-  grim
-  slurp
-  thunar
-  ripgrep
-  xdg-desktop-portal-wlr
-  xdg-desktop-portal-hyprland
-  xdg-desktop-portal
+	hyprland
+	hyprlock
+	wl-clipboard
+	wf-recorder
+	rofi-wayland
+	pavucontrol
+	hyprland-qtutils
+	dunst
+	swaybg
+	alacritty
+	hyprcursor
+	noto-fonts
+	hyprpicker
+	nwg-look
+	jq
+	gvfs
+	mpv
+	playerctl
+	pamixer
+	neovim
+	brightnessctl
+	power-profiles-daemon
+	waybar
+	papirus-icon-theme
+	starship
+	grim
+	slurp
+	thunar
+	ripgrep
+	xdg-desktop-portal-wlr
+	xdg-desktop-portal-hyprland
+	xdg-desktop-portal
 )
 
 # Function to install packages
 install_packages() {
-  echo "Updating package list..."
-  sudo pacman -Syy
+	echo "Updating package list..."
+	sudo pacman -Syy
 
-  echo "Installing packages..."
-  sudo pacman -S -y "${PACKAGES[@]}"
+	echo "Installing packages..."
+	sudo pacman -Syu -y "${PACKAGES[@]}"
 
-  echo "Installation complete!"
+	echo "Installation complete!"
 }
 
 # Run installation
